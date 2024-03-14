b55da7f6cc1fff4004e0642df5611531a5f03380
Tomer Soffair <65923349+TomerSof@users.noreply.github.com>
2024-03-14 09:36:43 +0200

    Update Equipment.py
    Equipment class - 2nd approach

--- Equipment.py
+++ Equipment.py
@@ -1,16 +1,8 @@
-from enum import Enum
+class Equipment():
+    def __init__(self ,ID, quantity, name):
+        self.ID = ID
+        self.quantity = quantity
+        self.name = name
+        
 
-
-class Equipment(Enum):
-    MASK = "Diving Mask"
-    FINS = "Diving Fins"
-    WETSUIT = "Wetsuit"
-    REGULATOR = "Regulator"
-    BCD = "Buoyancy Control Device"
-    DIVE_COMPUTER = "Dive Computer"
-    SNORKEL = "Snorkel"
-    WEIGHT_BELT = "Weight Belt"
-    TANK = "Scuba Tank"
-    COMPASS = "Dive Compass"
-    DEPTH_GAUGE = "Depth Gauge"
-    UNDERWATER_CAMERA = "Underwater Camera"
+    
