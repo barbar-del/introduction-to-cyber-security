975d0c05073a148155f1da8291f08e7f48ecb2e5
Tomer Soffair <65923349+TomerSof@users.noreply.github.com>
2024-03-14 09:34:25 +0200

    Update Diver.py
    Diver class - 2nd approach

--- Diver.py
+++ Diver.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import Person
+import Equipment
 
 class Rank(Enum):
     Beginner = 1
@@ -6,15 +8,17 @@ class Rank(Enum):
     Advanced = 3
     Expert = 4
 #the diver class get an id, name and rank, check if the rank is an instance of the Rank enum and if so  crate the diver
-class Diver:
-    def __init__(self, name, id, diver_rank):
-        self.name = name
-        self.id = id
+class Diver(Person):
+    def __init__(self, name, ID, age, num_of_dives, rank, union, equipment):
+        super().__init__(name, ID, age)
+        self.num_of_dives = num_of_dives
+        self.union = union
+        self.equipment = equipment
         # Ensure diver_rank is an instance of Rank
-        if isinstance(diver_rank, Rank):
-            self.diver_rank = diver_rank
+        if isinstance(rank, Rank):
+            self.rank = rank
         else:
-            raise ValueError("diver_rank must be an instance of Rank Enum")
+            raise ValueError("The rank must be an instance of Rank Enum")
 
     def get_diver_rank(self):
         """Return the diver's rank."""
@@ -26,3 +30,9 @@ class Diver:
             self.diver_rank = diver_rank
         else:
             print(f"Invalid rank: {diver_rank}. Rank must be an instance of Rank Enum.")
+
+    def update_rank(self):
+        return
+    
+    def buy_equipment(self, equipments: Equipment):
+        return
